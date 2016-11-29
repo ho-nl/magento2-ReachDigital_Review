a2276a224ad5d7a77fd4aedeb3c6d6b45e9c48b0
Jeroen van Leusden <jeroen@h-o.nl>
2016-11-29 14:42:44 +0100

    [KINGDO-67] Add install script

---
+++ src/Setup/InstallData.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Copyright © 2016 H&O E-commerce specialisten B.V. (http://www.h-o.nl/)
+ * See LICENSE.txt for license details.
+ */
+
+namespace Ho\Review\Setup;
+
+use Magento\Framework\Setup\InstallDataInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+
+class InstallData implements InstallDataInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $connection = $setup->getConnection();
+        $reviewConsideration = $setup->getConnection()->newTable('rating_consideration')
+            ->addColumn(
+                'consideration_id',
+                \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+                null,
+                ['identity' => true, 'unsigned' => true, 'nullable' => false, 'primary' => true],
+                'Review entity id'
+            )
+            ->addColumn(
+                'review_id',
+                \Magento\Framework\DB\Ddl\Table::TYPE_BIGINT,
+                null,
+                ['identity' => false, 'unsigned' => true, 'nullable' => false, 'primary' => false],
+                'Review id'
+            )
+            ->addColumn(
+                'type',
+                \Magento\Framework\DB\Ddl\Table::TYPE_SMALLINT,
+                null,
+                ['nullable' => false, 'default' => '0'],
+                'Type'
+            )
+            ->addColumn(
+                'value',
+                \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+                '64k',
+                ['nullable' => false],
+                'Value'
+            )
+            ->addIndex(
+                $connection->getIndexName('review_detail', ['review_id', 'type']),
+                ['review_id', 'type']
+            )
+            ->addForeignKey(
+                $connection->getForeignKeyName('rating_consideration', 'review_id', 'review', 'review_id'),
+                'review_id',
+                $connection->getTableName('review'),
+                'review_id',
+                \Magento\Framework\DB\Ddl\Table::ACTION_CASCADE
+            )
+            ->setComment('Review considerations');
+
+        $connection->createTable($reviewConsideration);
+    }
+}
